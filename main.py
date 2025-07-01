acfd774b0d18ac3baa05919983b5b154a3253cff
HudaNayyer <114230183+HudaNayyer@users.noreply.github.com>
2025-07-01 18:23:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import random
 
+def generate_random_ip():
+    return f"192.168.1.{random.randint(0,20)}"
+#defining the method that generates a random ip number, to represent source ip.
 
+def check_firewall_rules(ip, rules):
+    for rule_ip, action in rules.items():
+    #for loop to unpack the dictionary via items function for each IPS on blocklist
+        if ip == rule_ip:
+            return action
+        #compare randomly generated IP and see if it matches, and then it "blocks"
+    return "allow"
+    #allows if it does not match ip on ip blocklist.
+#defining the method to check firewall rules that receives IP and rules as an argument
+        
+    
 def main():
     firewall_rules = {
         #ip addresses not allowed on internal network
@@ -21,7 +35,8 @@ def main():
         random_number = random.randint(0,9999)
         #random variable between 0 - 9999 too serve as a unique identifier for when we
         # take multiple actions against a single IP, we have an distinguisher between those actions
-        print()
+        print(f"TP: {ip_address}, Action:{action}, Random: {random_number}")
+        #print ip address of the packet analyzed, the action taken against ip, and random unique iditenifier number
     
     if __name__ == "__main__":
         main()
